e65094f5cca6e5c03340107266225fda5d3d0924
Li Yuan <202070317@163.com>
2012-11-16 18:35:18 +0800

    db

--- src/com/four/dake/test/Test.java
+++ src/com/four/dake/test/Test.java
@@ -1,9 +1,11 @@
 package com.four.dake.test;
 
+import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
+import com.four.dake.db.DBHelp;
 import com.four.dake.entity.User;
 
 import net.sf.json.JSONObject;
@@ -11,22 +13,7 @@ import net.sf.json.JSONObject;
 public class Test {
 
 	public static void main(String[] args) {
-//		String str = "aaaaa";
-//		String s = str.replaceFirst(""+str.charAt(0), ""+(char)(str.charAt(0)-32));
-//		System.out.print(s);
-		
-		JSONObject json = new JSONObject();
-		
-		Map<String, String> userMap = new HashMap<String, String>();
-		userMap.put("id", "2");
-		userMap.put("name", "yangkit");
-		json.put("user", userMap);
-		JSONObject userJson = json.getJSONObject("user");
-		String id = userJson.getString("id");
-		userJson.remove("id");
-		System.out.println(id);
-		System.out.println(userJson);
-//		String str = user.toString();
-//		System.out.println(str);
+		Connection con = DBHelp.getConn();
+		System.out.println(con.toString());
 	}
 }
